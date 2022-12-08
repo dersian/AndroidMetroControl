9a95a1c404962f05b7264a7423fa5dc995f161a2
dersian <sam.dilmaghanian@gmail.com>
2022-12-08 23:21:47 +0100

    app keeps crashing

--- app/src/main/java/com/example/metrocontrol/MainActivity.java
+++ app/src/main/java/com/example/metrocontrol/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.metrocontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.controlmetro.R;
@@ -13,13 +16,33 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
 
+
+    private Button callActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.NearbyList);
-        baseAdapter adapterkke = new baseAdapter(getApplicationContext(), lijst, vehicleImages);
+        baseAdapter adapterkke = new baseAdapter(getApplicationContext(), lijst, vehicleImages);  //generates the customizable listview
         listView.setAdapter(adapterkke);
+
+        callActivity = (Button) findViewById(R.id.snitchButton);
+        callActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openReportControlActivity();
+            }
+        });
+
+
+    }
+
+    public void openReportControlActivity(){
+        Intent intent = new Intent(this, ReportControlActivity.class);
+        startActivity(intent);
     }
 }
+
