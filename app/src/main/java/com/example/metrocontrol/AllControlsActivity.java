9be395442193a870a431fec2cefedc114864c0e5
dersian <sam.dilmaghanian@gmail.com>
2022-12-10 00:27:40 +0100

    swipingFeature implement

--- app/src/main/java/com/example/metrocontrol/AllControlsActivity.java
+++ app/src/main/java/com/example/metrocontrol/AllControlsActivity.java
@@ -2,15 +2,36 @@ package com.example.metrocontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import com.example.controlmetro.R;
 
 public class AllControlsActivity extends AppCompatActivity {
+    float x1,y1,x2,y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_controls);
+    }
+
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 >  x2){
+                    Intent i = new Intent(AllControlsActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
     }
 }
